391642d6efff1964b2afcaa1e2cc5b0796c63342
thecatvoid <voidcat@tutanota.com>
2025-01-12 17:21:28 +0530

    Add text editor, set app name to CodeRunner, set DarkTheme

--- app/src/main/java/com/code/runner/MainActivity.java
+++ app/src/main/java/com/code/runner/MainActivity.java
@@ -2,16 +2,23 @@ package com.code.runner;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText("Hello, World!");
-        textView.setTextSize(24);
-        setContentView(textView);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        EditText editText = new EditText(this);
+        editText.setHint("Enter text here");
+
+        layout.addView(editText);
+
+        setContentView(layout);
     }
 }
 
