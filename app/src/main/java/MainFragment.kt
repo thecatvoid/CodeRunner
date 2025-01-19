5249057c80856bf5528ac62b3506d957d0dfbcf0
thecatvoid <voidcat@tutanota.com>
2025-01-19 21:12:03 +0530

    Modularized and added basic text editor

---
+++ app/src/main/java/MainFragment.kt
@@ -0,0 +1,42 @@
+package com.code.runner.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import com.code.runner.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val scrollView = ScrollView(context)
+        val linearLayout = LinearLayout(context)
+        linearLayout.orientation = LinearLayout.VERTICAL
+
+        val editText = EditText(context)
+        editText.hint = "Enter text here"
+        editText.inputType = android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_TEXT_FLAG_MULTI_LINE
+        editText.minLines = 3
+        editText.maxLines = Integer.MAX_VALUE
+        editText.setPadding(16, 16, 16, 16)
+        editText.isVerticalScrollBarEnabled = true
+        editText.setScroller(android.widget.Scroller(context))
+        editText.isScrollContainer = true
+
+        linearLayout.addView(editText)
+        scrollView.addView(linearLayout)
+
+        return scrollView
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+}
---
+++ app/src/main/java/com/code/runner/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.code.runner
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.code.runner.ui.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
