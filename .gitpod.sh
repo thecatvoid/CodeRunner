090e2a21ef3353accb331ada6f9a474dd16770cd
thecatvoid <voidcat@tutanota.com>
2025-01-27 08:27:27 +0000

    setup nvim

--- .gitpod.sh
+++ .gitpod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y openjdk-17-jdk wget curl unzip git neovim
+sudo apt install -y openjdk-17-jdk wget curl unzip git tmux
 sudo apt-get update
 sudo mkdir /opt/android-sdk
 sudo chown -R $USER:$USER /opt/
@@ -8,11 +8,12 @@ wget "https://dl.google.com/android/repository/commandlinetools-linux-11076708_l
 unzip latest.zip
 rm -f latest.zip
 mv cmdline-tools /opt/
-export PATH="/opt/cmdline-tools/bin:$PATH"
+export PATH="/opt/cmdline-tools/bin:/opt/nvim-linux64/bin:$PATH"
 yes | sdkmanager --licenses --sdk_root=/opt/android-sdk
 sdkmanager --sdk_root=/opt/android-sdk "platform-tools" "build-tools;30.0.3" "platforms;android-30"
 cd $HOME
 git clone https://www.github.com/thecatvoid/dot
 cp -a dot/.* ./
+curl -L -o - https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | tar -xzf - -C /opt/
 nvim -c 'PlugInstall' -c 'qa'
 nvim -c 'TSInstall kotlin' -c 'qa'
